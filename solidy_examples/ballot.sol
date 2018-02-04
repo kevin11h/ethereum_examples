ad1ded5b061e53967b4f0408bb9951eb0c373058
matr1xc0in <34523785+matr1xc0in@users.noreply.github.com>
2018-02-03 23:21:41 -0800

    Enhance ballot.sol example

--- solidy_examples/ballot.sol
+++ solidy_examples/ballot.sol
@@ -1,65 +1,109 @@
 pragma solidity ^0.4.0;
+
 contract Ballot {
 
-    struct Voter {
-        uint weight;
-        bool voted;
-        uint8 vote;
-        address delegate;
-    }
+    // A struct for a single `Proposal`
     struct Proposal {
-        uint voteCount;
+        bytes32 name; // Proposal name up to 32 bytes
+        uint64 voteCount; // accumulated vote counts
     }
 
-    address chairperson;
-    mapping(address => Voter) voters;
-    Proposal[] proposals;
+    // A single `Voter` struct.
+    struct Voter {
+        uint64 weight; // accumulated by delegation 
+        bool voted; // determine if the voter has voted or not
+        uint vote; // INDEX of the voted Proposal
+        address delegate; // person delegated to
+    }
 
-    /// Create a new ballot with $(_numProposals) different proposals.
-    function Ballot(uint8 _numProposals) public {
+    address public chairperson;
+
+    // Stores a Voter struct for each possible address
+    mapping(address => Voter) public voters;
+
+    // a dynamically sized array of Proposal struct
+    Proposal[] public proposals;
+
+    // Create a new ballot for each proposals with name `proposalNames`
+    function Ballot(bytes32[] proposalNames) public {
         chairperson = msg.sender;
-        voters[chairperson].weight = 1;
-        proposals.length = _numProposals;
+        voters[chairperson].weight = 1; // default vote weight is 1, fixed
+        
+        for (uint i = 0; i < proposalNames.length; i++) {
+            // Create a temp Proposal struct and append to the end of proposals array
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
     }
 
-    /// Give $(toVoter) the right to vote on this ballot.
-    /// May only be called by $(chairperson).
+    // Give `toVoter` the right to vote on this ballot.
+    // May only be called by `chairperson`.
     function giveRightToVote(address toVoter) public {
-        if (msg.sender != chairperson || voters[toVoter].voted) return;
-        voters[toVoter].weight = 1;
+        if (msg.sender == chairperson && !voters[toVoter].voted && (voters[toVoter].weight == 0)) {
+            voters[toVoter].weight = 1;
+        }
     }
 
-    /// Delegate your vote to the voter $(to).
+    /// Delegate your vote to the voter `to`.
     function delegate(address to) public {
+        // this assigns `sender` as a reference to `voters[msg.sender]`
         Voter storage sender = voters[msg.sender]; // assigns reference
-        if (sender.voted) return;
-        while (voters[to].delegate != address(0) && voters[to].delegate != msg.sender)
+        // require(!sender.voted);
+        if (sender.voted) {
+            return;
+        }
+        
+        // self-delegation is not allowed
+        // require(to != msg.sender);
+        if (to == msg.sender) {
+            return;
+        }
+
+        while (voters[to].delegate != address(0)) {
             to = voters[to].delegate;
-        if (to == msg.sender) return;
+
+            require(to != msg.sender);
+        }
         sender.voted = true;
         sender.delegate = to;
+        // Pass it on to the next delegate (block chain concept)
         Voter storage delegateTo = voters[to];
-        if (delegateTo.voted)
+        if (delegateTo.voted) {
             proposals[delegateTo.vote].voteCount += sender.weight;
-        else
+        } else {
             delegateTo.weight += sender.weight;
+        }
     }
 
-    /// Give a single vote to proposal $(toProposal).
-    function vote(uint8 toProposal) public {
+    /// Give your vote including delegated vote to you to the proposal `proposals[toProposal]`.
+    function vote(uint toProposal) public {
         Voter storage sender = voters[msg.sender];
-        if (sender.voted || toProposal >= proposals.length) return;
+        if (sender.voted) {
+            return;
+        }
         sender.voted = true;
         sender.vote = toProposal;
+        // According to Solidity doc, if the index is out of range, it will automatically 
+        // throw (fail) and revert all changes.
         proposals[toProposal].voteCount += sender.weight;
     }
 
-    function winningProposal() public constant returns (uint8 _winningProposal) {
-        uint256 winningVoteCount = 0;
-        for (uint8 prop = 0; prop < proposals.length; prop++)
+    function winningProposal() public view returns (uint _winningProposal) {
+        // use larger uint here since we may have a lot of voters, billion and trillions, etc.
+        // this is million+ times more than the population on earth.
+        uint64 winningVoteCount = 0;
+
+        for (uint prop = 0; prop < proposals.length; prop++) {
             if (proposals[prop].voteCount > winningVoteCount) {
                 winningVoteCount = proposals[prop].voteCount;
                 _winningProposal = prop;
             }
+        }
+    }
+
+    function winnerName() public view returns (bytes32 _winnerName) {
+        _winnerName = proposals[winningProposal()].name;
     }
 }
