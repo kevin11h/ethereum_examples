15d59f25143ab57a654e54a5f66b09958e6e1d61
matr1xc0in <34523785+matr1xc0in@users.noreply.github.com>
2018-02-14 16:47:45 -0800

    Fix unit typo

--- solidy_examples/cointransfer.sol
+++ solidy_examples/cointransfer.sol
@@ -4,9 +4,9 @@ contract Coin {
     
     address public minter;
 
-    mapping (address => unit) public balances;
+    mapping (address => uint) public balances;
 
-    event Sent(address from, address to, unit amount);
+    event Sent(address from, address to, uint amount);
 
     function Coin() public {
         minter = msg.sender;
@@ -19,7 +19,7 @@ contract Coin {
         balances[receiver] += amount;
     }
 
-    function send(address receiver, unit amount) public {
+    function send(address receiver, uint amount) public {
         if (balances[msg.sender] < amount) {
             return;
         }
